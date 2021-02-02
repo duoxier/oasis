a03faf00fb43e64dc1db7f33d3ecd85ee1affa43
duoxier <imamba@126.com>
2021-02-02 15:35:05 +0800

    增加tools

---
+++ src/main/java/com/weibo/oasis/vo/NearbyUserVO.java
@@ -0,0 +1,14 @@
+package com.weibo.oasis.vo;
+
+public class NearbyUserVO {
+
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
