6ee8111fff1ba079e0c2f5ddeacb8f00c6fb2323
duoxier <imamba@126.com>
2021-02-18 17:39:33 +0800

    增加tools

---
+++ src/main/java/com/weibo/oasis/vo/TimelineUserVO.java
@@ -0,0 +1,50 @@
+package com.weibo.oasis.vo;
+
+public class TimelineUserVO {
+
+    private Integer uid;
+    private String name;
+    private Integer followerCount;  //粉丝
+    private Integer followingCount; //关注
+    private String avatar; //头像
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getFollowerCount() {
+        return followerCount;
+    }
+
+    public void setFollowerCount(Integer followerCount) {
+        this.followerCount = followerCount;
+    }
+
+    public Integer getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(Integer followingCount) {
+        this.followingCount = followingCount;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
